3323bb7322c6007ee0e0bef04aece9ce60db7b39
Liy <2516387949@qq.com>
2022-08-11 22:22:33 +0800

    删除无用代码

--- Calculator/AppCalculator.cpp
+++ Calculator/AppCalculator.cpp
@@ -20,8 +20,6 @@
 #include "Oval.h"
 #include "Triangle.h"
 
-#define Condition 0
-
 using namespace std;
 
 int main() {
@@ -45,48 +43,27 @@ int main() {
             cin >> length;
             cout << "请输入长方形的宽：" << endl;
             cin >> width;
-#if Condition
-            GeometricFigure *pRect = new Rectangle(length, width);
-            //Rectangle rect(length, width);
 
-            cout << "长方形的面积是：" << pRect->GetArea() << endl;
-            cout << "长方形的周长是：" << pRect->GetPerimeter() << endl;
-            delete pRect;
-            pRect = nullptr;
-#else
             pGF = new Rectangle(length, width);
-#endif
+
             break;
         }
         case Square2:
         {
             cout << "请输入正方形的边长：" << endl;
             cin >> length;
-#if Condition
-            GeometricFigure *pSquare = new Square(length);
-            cout << "正方形的面积是：" << pSquare->GetArea() << endl;
-            cout << "正方形的周长是：" << pSquare->GetPerimeter() << endl;
-            delete pSquare;
-            pSquare = nullptr;
-#else
+
             pGF = new Square(length);
-#endif
+
             break;
         }
         case Circle3:
         {
             cout << "请输入圆形的半径：" << endl;
             cin >> radius;
-#if Condition
-            //Circle circle(radius);
-            GeometricFigure *pCircle = new Circle(radius);
-            cout << "圆形的面积是：" << pCircle->GetArea() << endl;
-            cout << "圆形的周长是：" << pCircle->GetPerimeter() << endl;
-            delete pCircle;
-            pCircle = nullptr;
-#else
+
             pGF = new Circle(radius);
-#endif
+
             break;
         }
         case Oval4:
@@ -95,17 +72,9 @@ int main() {
             cin >> length;
             cout << "请输入椭圆形的短半轴：" << endl;
             cin >> width;
-#if Condition
-            GeometricFigure *pOval = new Oval(length, width);
-            //Rectangle rect(length, width);
 
-            cout << "椭圆形的面积是：" << pOval->GetArea() << endl;
-            cout << "椭圆形的周长是：" << pOval->GetPerimeter() << endl;
-            delete pOval;
-            pOval = nullptr;
-#else
             pGF = new Circle(radius);
-#endif
+
             break;
         }
         case Triangle5:
@@ -121,16 +90,9 @@ int main() {
                     cout << "输入有误请重新输入" << endl;
                 }
             } while (result == false);
-#if Condition
-            GeometricFigure *Tri = new Triangle(a, b, c);
-            cout << "三角形的周长是：" << Tri->GetPerimeter() << endl;
-            cout << "三角形的面积是：" << Tri->GetArea() << endl;
 
-            delete Tri;
-            Tri = nullptr;
-#else
             pGF = new Triangle(a, b, c);
-#endif
+
             break;
         }
 
